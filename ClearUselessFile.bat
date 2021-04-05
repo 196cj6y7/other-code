8c71cb2a5da82918d822cca8e319df68382056e0
196cj6y7 <43287205+196cj6y7@users.noreply.github.com>
2021-04-05 23:01:38 +0800

    Create ClearUselessFile.bat

---
+++ ClearUselessFile.bat
@@ -0,0 +1,27 @@
+@echo off 
+
+echo 正在清除系統垃圾文件，請稍等...... 
+
+del /f /s /q %systemdrive%\*.tmp
+del /f /s /q %systemdrive%\*._mp
+del /f /s /q %systemdrive%\*.log
+del /f /s /q %systemdrive%\*.gid
+del /f /s /q %systemdrive%\*.chk
+del /f /s /q %systemdrive%\*.old
+del /f /s /q %systemdrive%\recycled\*.*
+del /f /s /q %windir%\*.bak
+del /f /s /q %windir%\prefetch\*.*
+del /f /q %userprofile%\cookies\*.*
+del /f /q %userprofile%\recent\*.*
+del /f /s /q /a D:\*.torrent
+del /f /s /q /a X:\*.torrent
+del /f /s /q "%userprofile%\Local Settings\Temporary Internet Files\*.*"
+del /f /s /q "%userprofile%\Local Settings\Temp\*.*"
+del /f /s /q "%userprofile%\recent\*.*"
+DEL /S /F /Q "%systemroot%\Temp\*.*"
+DEL /S /F /Q "%AllUsersProfile%\「開始」功能表\程式集\Windows Messenger.lnk"
+
+echo 清除系統垃圾檔案完成！
+echo 清除系統完成！ 
+
+echo. & pause
