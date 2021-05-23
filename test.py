eb0cfd6d6ea48a4becb1d8d5845f6dc9b27b1dad
196cj6y7 <43287205+196cj6y7@users.noreply.github.com>
2021-05-23 16:17:23 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,36 @@
+'''
+    'date' =>'date - n days'
+     type : str => str
+'''
+
+import time
+import calendar
+from datetime import datetime
+
+def getDaysBefore(date, dds):
+    import datetime
+    datetime_dt = str_to_datetime(date)
+    time_delta = datetime.timedelta(days = dds) 
+    new_dt = datetime_dt - time_delta 
+    datetime_format = new_dt.strftime("%Y%m%d") 
+    return datetime_format
+
+def str_to_datetime(date):
+    from datetime import datetime
+    date_new_format = datetime.strptime(date, "%Y%m%d")
+    return date_new_format
+
+#-----------------------------------------------------1232211
+'''
+print(type(time_delta))
+print(type(datetime_dt))
+print(type(datetime_format))
+'''
+
+if __name__ == '__main__':
+    #today1 = datetime.datetime.today()
+    #today1 = datetime.strptime('20210520', "%Y%m%d")
+    today1 = '20210520'
+    print(today1)
+    final = getDaysBefore(today1, 8) # 8 days before
+    print(final)
