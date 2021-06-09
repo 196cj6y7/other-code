2e80e8185d9d1940e68e49085cccd2440a1a5b98
196cj6y7 <43287205+196cj6y7@users.noreply.github.com>
2021-06-09 21:13:43 +0800

    Create no_log_browser.py

---
+++ no_log_browser.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import os
+import time
+
+#open Firefox browser headless
+opts = Options()
+opts.set_headless(headless=True)
+assert opts.headless
+
+# no log file setting
+browser = webdriver.Firefox(options=opts,service_log_path=os.devnull)
+# service_log_path = os.devnull makes no geckodriver.log file
+
+url = 'https://www.google.com.tw/'
+time.sleep(3)
+
+browser.quit()
